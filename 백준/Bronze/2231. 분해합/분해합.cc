5b3b25dc36ab0b3d745c3a654a6594789f416375
Eajmuk <137264384+Eajmuk@users.noreply.github.com>
2026-03-08 23:12:53 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = i;
+		int sum = i;
+
+		while (temp > 0)
+		{
+			sum += temp % 10;
+			temp /= 10;
+		}
+
+		if (sum == n) {
+			cout << i;
+			return 0;
+		}
+
+	}
+	cout << 0;
+	return 0;
+}
